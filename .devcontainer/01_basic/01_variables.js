be6de28786c9a6eaba84adc5c4e8ea88b66fc84f
ZAIN1125 <zainkhanam455@gmail.com>
2024-07-21 16:49:43 +0000

    container config

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,6 @@
+const accountId= 1122
+let accountemail="zain@gmail.com"
+var accountpass="2323"
+acoountcity="multan"
+let accountstate ;
+console.table([accountId,accountemail,accountpass,acoountcity,accountstate])
